7446ff686bebc3492c57f4f8cbb8cae4eb6573ea
exposit <forgepoet@gmail.com>
2015-10-26 21:05:14 -0500

    Update gamedb.py

--- gamedb.py
+++ gamedb.py
@@ -3,9 +3,9 @@
 # format is game id (title) : then game file, interpreter, args:'None', title, author, blurb, genre
 game_list = {
 # frotz games
-    '905': {'file':"frotz/905.z5", 'interpreter': "dfrotz", 'args':"None", 'title':'9:05', 'author':'Adam Cadre', 'blurb':"You're woken up by the telephone, having slept a lot longer than you should have, and there'll be hell to pay if you don't get out of the house in a hurry.", 'genre':"slice of life"},
+    '905': {'file':"905.z5", 'interpreter': "dfrotz", 'args':"None", 'title':'9:05', 'author':'Adam Cadre', 'blurb':"You're woken up by the telephone, having slept a lot longer than you should have, and there'll be hell to pay if you don't get out of the house in a hurry.", 'genre':"slice of life"},
 # tads games
-    #'gunmute': {'file':"tads/gunmute.t3", 'interpreter': "frob", 'args':"-i plain", 'title':'Gun Mute', 'author':'C.E.Pacian', 'blurb':"Step into the shoes of Mute Lawton, a lone cowboy who must stop an execution set to occur at noon by shooting his way past dangerous cyborgs and mutants in a post-apocalyptic western setting.", 'genre':'western'},
+    #'gunmute': {'file':"gunmute.t3", 'interpreter': "frob", 'args':"-i plain", 'title':'Gun Mute', 'author':'C.E.Pacian', 'blurb':"Step into the shoes of Mute Lawton, a lone cowboy who must stop an execution set to occur at noon by shooting his way past dangerous cyborgs and mutants in a post-apocalyptic western setting.", 'genre':'western'},
 #glulxe games
-    #'headless': {'file':"glulxe/Headless.gblorb", 'interpreter': "glulxe", 'args':"None", 'title':'Headless, Hapless', 'author':'Geoff Moore', 'blurb':"It's happened again. You've had a great night riding around, freaking everyone out with your whole headless horseman thing, and you're about to set off for home when you realise you've lost your head. Well, it can't have gotten far... ", 'genre':"Halloween"},
+    #'headless': {'file':"Headless.gblorb", 'interpreter': "glulxe", 'args':"None", 'title':'Headless, Hapless', 'author':'Geoff Moore', 'blurb':"It's happened again. You've had a great night riding around, freaking everyone out with your whole headless horseman thing, and you're about to set off for home when you realise you've lost your head. Well, it can't have gotten far... ", 'genre':"Halloween"},
 }
